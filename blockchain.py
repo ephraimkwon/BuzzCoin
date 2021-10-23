336a9971d46546b430fdb30afa6db3ac8d0548e4
ephraimkwon <epjkwon.11@gmail.com>
2021-10-23 15:45:21 -0400

    update blockchain
    added transaction stuff

--- blockchain.py
+++ blockchain.py
@@ -15,6 +15,7 @@ class Blockchain (object):
         self.pending_transactions = []
         self.miner_reward = 10
         self.nodes = {}
+        self.starting_amount = 100
 
     def get_last_block(self):
         return self.chain[-1]
@@ -40,7 +41,7 @@ class Blockchain (object):
     def get_last_block(self):
         return self.chain[-1]
 
-    def chainJSONencode(self): # encoding a chain object into a JSON we can use on the web
+    def chain_JSON_encode(self): # encoding a chain object into a JSON we can use on the web
         blockArrJSON = []
         for block in self.chain:
             blockJSON = {}
@@ -54,19 +55,19 @@ class Blockchain (object):
                 tJSON['time'] = transaction.time
                 tJSON['sender'] = transaction.sender
                 tJSON['receiver'] = transaction.receiver
-                tJSON['amt'] = transaction.amt
+                tJSON['amount'] = transaction.amount
                 tJSON['hash'] = transaction.hash
                 transactionsJSON.append(tJSON)
             blockJSON['transactions'] = transactionsJSON
             blockArrJSON.append(blockJSON)
         return blockArrJSON
 
-    def chainJSONdecode(self, chainJSON): # decoding a chainJSON into Blockchain object
-        chain=[]
+    def chain_JSON_decode(self, chainJSON): # decoding a chainJSON from the web into Blockchain object
+        chain = []
         for blockJSON in chainJSON:
             tArr = []
             for tJSON in blockJSON['transactions']:
-                transaction = Transaction(tJSON['sender'], tJSON['receiver'], tJSON['amt'])
+                transaction = Transaction(tJSON['sender'], tJSON['receiver'], tJSON['amount'])
                 transaction.time = tJSON['time']
                 transaction.hash = tJSON['hash']
                 tArr.append(transaction)
@@ -76,25 +77,61 @@ class Blockchain (object):
             block.nonse = blockJSON['nonse']
             chain.append(block)
         return chain
+
+    def get_balance(self, user):
+        balance = 0
+        for i in range(1, len(self.chain)):
+            block = self.chain[i]
+            # need try except because there might not be a transaction of this particular user at each block
+            try:
+                for transaction in block.transactions:
+                    if transaction.sender == user:
+                        balance -= transaction.amount
+                    if transaction.receiver == user:
+                        balance += transaction.amount
+            except:
+                print("no transaction here")
+
+        return balance + self.starting_amount
     
     def generate_keys(self): # generates the private and public keys
         key = RSA.generate(2048)
         private_key = key.exportKey()
+        # creates private key file
         file_out = open("private.pem", "wb")
         file_out.write(private_key)
 
         public_key = key.public_key().export_key()
-        file_out = open("receiver.pem", "wb")
+        # creates public key file
+        file_out = open("public.pem", "wb")
         file_out.write(public_key)
 
         return key.public_key().exportKey().decode('ASCII') # returns an ascii decoded public keys
 
         
 
-    def add_transaction(self, sender, receiver, amt, keystring, senderkey):
-        pass
+    def add_transaction(self, sender, receiver, amount, keystring, sender_key):
+        key_byte = keystring.encode("ASCII")
+        sender_key_byte = sender_key.encode("ASCII")
 
-    
+        key = RSA.import_key(key_byte)
+        sender_key = RSA.import_key(sender_key_byte)
+
+        if not sender or not receiver or not amount: # checks if any of the inputs are null
+            print("Transaction error: Null error")
+            return False
+        
+        # creating and signing the transaction
+        transaction = Transaction(sender, receiver, amount)
+        transaction.sign_transaction(key, sender_key)
+        
+        if not transaction.is_valid_transaction():
+            print("Transaction error: not valid transaction")
+            return False
+        
+        self.pending_transactions.append(transaction)
+
+        return len(self.chain) + 1
 
 class Block (object):
     def __init__(self, transactions, time, index):
@@ -116,9 +153,6 @@ class Block (object):
     
     def JSONencode(self): # returns a json version of itself
         return jsonpickle.encode(self)
-    
-    def JSONdecode(self):
-        return jsonpickle.decode(self)
     
     def mine_block(self):
         
@@ -143,15 +177,15 @@ class Block (object):
 
 
 class Transaction (object):
-    def __init__(self, sender, receiver, amt):
+    def __init__(self, sender, receiver, amount):
         self.sender = sender
         self.receiver = receiver
-        self.amt = amt
+        self.amount = amount
         self.time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
         self.hash = self.calculate_hash()
 
     def calculate_hash(self):
-        hashString = self.sender + self.receiver + str(self.amt) + str(self.time)
+        hashString = self.sender + self.receiver + str(self.amount) + str(self.time)
         hashEncoded = json.dumps(hashString, sort_keys=True).encode()
         return hashlib.sha256(hashEncoded).hexdigest()
     
@@ -184,5 +218,6 @@ class Transaction (object):
 
 
 blockchain = Blockchain()
-pprint(blockchain.chainJSONencode())
+pprint(blockchain.chain_JSON_encode())
+blockchain.generate_keys()
 blockchain.get_last_block().mine_block()
