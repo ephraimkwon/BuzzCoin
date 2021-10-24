01cdbc02164bf7feb3d4bc03ed9ae14c9d981839
Evan Ding <eding32@gatech.edu>
2021-10-23 20:13:22 -0400

    update model

---
+++ buzzcoin/models.py
@@ -0,0 +1,12 @@
+from buzzcoin import db;
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(35), unique = False, nullable=False)
+    username = db.Column(db.String(15), unique = True, nullable=False)
+    email = db.Column(db.String(100), unique = True, nullable=False)
+    password = db.Column(db.String(120), unique = False, nullable=False)
+    key = db.Column(db.String(100000), unique = True, nullable=False)
+
+def __repr__(self):
+        return f"User('{self.name}', '{self.username}', '{self.email}')"
---
+++ buzzcoin/routes.py
@@ -0,0 +1,23 @@
+from __init__ import app
+from flask import render_template
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/<username>')
+def userpage(username):
+    return f'<h1>About {username}</h1>'
+
+@app.route('/blockchain')
+def blockchain_page():
+    items = [
+    {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+    {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+    {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template('blockchain.html', items = items)
+
+@app.route('/mine')
+def mine_page():
+    return render_template('mine.html')
